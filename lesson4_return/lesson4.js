304b582c93dd33f06b2883a7baa9d1549194d84a
AndreyLukashev <energy2024@internet.ru>
2024-02-18 11:57:05 +0300

    add arrev in reverse

--- lesson4_return/lesson4.js
+++ lesson4_return/lesson4.js
@@ -2,9 +2,9 @@
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(arr);
-let arrrev = [];
 
-const reverse = (arr) => {    
+const reverse = (arr) => {  
+     const arrrev = [];  
 for (let i = (arr.length - 1); i>=0; i-- ) {
     arrrev.push(arr[i])};
     console.log(arrrev);
